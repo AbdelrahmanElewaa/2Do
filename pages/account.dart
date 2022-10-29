e21c84987b7f0d0737c8fba35888eee9abd7f2d7
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-10-29 03:08:15 +0200

    Settings-Account pages

---
+++ pages/account.dart
@@ -0,0 +1,155 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Account extends StatelessWidget {
+  const Account({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 12, 34, 51),
+      body: SafeArea(
+        child: Column(children: [
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Account',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 40, color: Colors.amber),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 30),
+            child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Stack(
+                children: [
+                  CircleAvatar(
+                    radius: 45,
+                    backgroundImage: AssetImage('assets/omar.jpg'),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print("Settings");
+                    },
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(60, 60, 0, 0),
+                      decoration: BoxDecoration(
+                          color: Colors.amber, shape: BoxShape.circle),
+                      child: Icon(
+                        Icons.mode_edit_outline,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text('Omar mohamed abdel',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Color.fromARGB(255, 255, 192, 69),
+                    )),
+              )
+            ]),
+          ),
+          // ignore: prefer_const_literals_to_create_immutables
+
+          Divider(
+            height: 50,
+            thickness: 1,
+          ),
+          Expanded(
+            child: ListView(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                ProfileListItem(
+                  header: 'Name',
+                  text: 'Omar mohamed',
+                ),
+                ProfileListItem(
+                  header: 'Username',
+                  text: '@Omarr',
+                ),
+                ProfileListItem(
+                  header: 'Password',
+                  text: 'sdahsdbasj',
+                ),
+                ProfileListItem(
+                  header: 'Mobile Number',
+                  text: '01287121314',
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class ProfileListItem extends StatelessWidget {
+  final header;
+  final text;
+  const ProfileListItem({super.key, required this.header, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15),
+      child: Container(
+        height: 80,
+        margin: EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Color.fromARGB(255, 255, 255, 255),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  header,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+              ]),
+              Row(children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  text,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+                Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    print('hello');
+                  },
+                  child: Text('Update',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 10, 145, 171))),
+                ),
+              ]),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ pages/settings.dart
@@ -0,0 +1,137 @@
+// ignore_for_file: prefer_const_constructors, prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({super.key});
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Scaffold(
+        backgroundColor: Color.fromARGB(255, 12, 34, 51),
+        body: SafeArea(
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    CircleAvatar(
+                      radius: 70,
+                      backgroundImage: AssetImage('assets/1024.png'),
+                    ),
+                  ]),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              Text('Omar mohamed abdel',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
+                    color: Color.fromARGB(255, 255, 192, 69),
+                  ))
+            ]),
+            Divider(
+              height: 50,
+              thickness: 1,
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).go('/account');
+                    },
+                    child: ProfileListItem(
+                      icon: Icons.account_box_rounded,
+                      text: 'Account',
+                    ),
+                  ),
+                  ProfileListItem(
+                    icon: Icons.production_quantity_limits,
+                    text: 'privacy',
+                  ),
+                  ProfileListItem(
+                    icon: Icons.feedback_outlined,
+                    text: 'Feedback',
+                  ),
+                  ProfileListItem(
+                    icon: Icons.login_rounded,
+                    text: 'Logout',
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
+    ]);
+  }
+}
+
+class ProfileListItem extends StatelessWidget {
+  final IconData icon;
+  final text;
+  const ProfileListItem({super.key, required this.icon, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15),
+      child: Container(
+        height: 70,
+        margin: EdgeInsets.symmetric(horizontal: 30),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.amber,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Row(children: [
+            Icon(
+              icon,
+              size: 40,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            Spacer(),
+            Icon(
+              Icons.arrow_forward_ios,
+              size: 30,
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class FeedBack extends StatelessWidget {
+  const FeedBack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('AlertDemo with TextField '),
+      content: TextField(
+        decoration: InputDecoration(hintText: "Enter Text"),
+      ),
+    );
+  }
+}
