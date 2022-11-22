e4d42e65345a75f66edc575caf2e7b94d6014834
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-11-22 23:18:02 +0200

    new nav bar

--- pages/home_page.dart
+++ pages/home_page.dart
@@ -1,12 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:go_router/go_router.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:todo/pages/home.dart';
 import 'package:todo/pages/stat.dart';
 import 'package:todo/util/tasks_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/util/colorextension.dart';
-import '../util/settingsbutton.dart';
+
 import 'notes_screen.dart';
 
 class HomePage extends StatefulWidget {
@@ -40,14 +41,13 @@ class _HomePageState extends State<HomePage> {
       drawer: Drawer(
         child: ListView(
           children: [
-        ListTile(
-        leading: const Icon(Icons.person),
-        title: const Text(' My Profile '),
-        onTap: () {
-          GoRouter.of(context).go('/settings');
-        },
-      ),
-
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text(' My Profile '),
+              onTap: () {
+                GoRouter.of(context).go('/settings');
+              },
+            ),
             ListTile(
               leading: const Icon(Icons.edit),
               title: const Text(' Edit Profile '),
@@ -55,50 +55,54 @@ class _HomePageState extends State<HomePage> {
                 GoRouter.of(context).go('/account');
               },
             ),
-      ListTile(
-        leading: const Icon(Icons.login),
-        title: const Text(' Login '),
-        onTap: () {
-          GoRouter.of(context).go('/login');
-        },
-      ),
-
-
-      ListTile(
-        leading: const Icon(Icons.logout),
-        title: const Text('LogOut'),
-        onTap: () {
-          Navigator.pop(context);
-        },
-      ),
+            ListTile(
+              leading: const Icon(Icons.login),
+              title: const Text(' Login '),
+              onTap: () {
+                GoRouter.of(context).go('/login');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('LogOut'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
           ],
         ),
       ),
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: const Icon(color: Colors.black, Icons.home_sharp),
-            // ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(color: Colors.black, Icons.calendar_month_rounded),
-            // ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(color: Colors.black, Icons.book),
-            label: '',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-        backgroundColor: Color.fromARGB(140, 255, 193, 7),
-      ),
       body: Center(
         child: _pages.elementAt(_selectedIndex), //New
+      ),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+          child: GNav(
+              haptic: true,
+              backgroundColor: Colors.black,
+              color: Colors.white,
+              activeColor: Colors.white,
+              tabBackgroundColor: Colors.grey.shade500,
+              gap: 8,
+              curve: Curves.easeInSine,
+              duration: Duration(milliseconds: 900),
+              padding: EdgeInsets.all(16),
+              tabs: const [
+                GButton(icon: Icons.home, text: 'Home'),
+                GButton(icon: Icons.calendar_month_rounded, text: 'Calendar'),
+                GButton(icon: Icons.add, text: 'New Task'),
+                GButton(icon: Icons.book, text: 'Notes'),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              }),
+        ),
       ),
     );
   }
