7c1d967cc3c7a52d0eb592a025854811bb462be3
Omar Mohamed Abdelrehim <omar1901044@miuegypt.edu.eg>
2022-11-23 20:24:07 +0200

    new notes screen

--- pages/notes_screen.dart
+++ pages/notes_screen.dart
@@ -2,273 +2,130 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/Model/note_model.dart';
 import 'package:intl/intl.dart';
+import 'package:go_router/go_router.dart';
+import 'package:todo/util/colorextension.dart';
+import 'package:todo/util/settingsbutton.dart';
+import '../util/drawerr.dart';
+import '../util/tasks_tile.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:calendar_timeline/calendar_timeline.dart';
+import '../util/calenderweek.dart';
+import '../util/searchbar.dart';
 
 // ignore_for_file: prefer_const_constructors
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
-class NotesScreen extends StatefulWidget {
-  const NotesScreen({super.key});
+class NotesScreen extends StatelessWidget {
+  NotesScreen({super.key});
 
-  @override
-  State<NotesScreen> createState() => _NotesScreenState();
-}
-
-class _NotesScreenState extends State<NotesScreen>
-    with SingleTickerProviderStateMixin {
-  int _selectedCategoryIndex = 0;
-  late TabController _tabController;
-  final DateFormat _dateFormatter = DateFormat('dd MMM');
-  final DateFormat _timeFormatter = DateFormat('h:mm');
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(initialIndex: 0, length: 1, vsync: this);
-  }
-
-  Widget _buildCategoryCard(int index, String title, int count) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _selectedCategoryIndex = index;
-        });
-      },
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
-        height: 240.0,
-        width: 175.0,
-        decoration: BoxDecoration(
-          color: _selectedCategoryIndex == index
-              ? Colors.amber
-              : Color(0xFFF5F7FB),
-          borderRadius: BorderRadius.circular(20.0),
-          boxShadow: [
-            _selectedCategoryIndex == index
-                ? BoxShadow(
-                    color: Colors.black26,
-                    offset: Offset(0, 2),
-                    blurRadius: 10.0)
-                : BoxShadow(color: Colors.transparent),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                title,
-                style: TextStyle(
-                  color: _selectedCategoryIndex == index
-                      ? Colors.white
-                      : Color(0xFFAFB4C6),
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                count.toString(),
-                style: TextStyle(
-                  color: _selectedCategoryIndex == index
-                      ? Colors.white
-                      : Colors.black,
-                  fontSize: 35.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
+  String formattedDate = DateFormat('MM-yyyy').format(DateTime.now());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          SizedBox(height: 40.0),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: 50.0,
-                  width: 50.0,
-                  decoration: BoxDecoration(
-                    // image: DecorationImage(
-                    //   image: AssetImage('assets/images/user.png'),
-                    // ),
-                    borderRadius: BorderRadius.circular(10.0),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        drawer: Drawerr(),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            //greating Row
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                children: [
+                  
+                  //Search Bar
+                  SearchBar(),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(25),
+                color: Colors.transparent,
+                child: Center(
+                  child: Column(
+                    children: [
+                      // Excercise Heading
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Tasks',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                                color: Color.fromARGB(255, 12, 34, 51)),
+                          ),
+
+                          //ADD Button
+                          ElevatedButton(
+                            onPressed: () {
+                              GoRouter.of(context).go('/addtask');
+                            },
+                            child: Icon(
+                              Icons.add,
+                              size: 40,
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                shape: CircleBorder(),
+                                backgroundColor: Colors.red),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      //Tasks Dashboard
+                      Expanded(
+                          child: ListView(
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: [
+                          TasksTile(
+                            icon: Icons.work,
+                            taskName: 'Project Meeting',
+                            subTitle: 'Emergency',
+                            color: Colors.blue,
+                          ),
+                          TasksTile(
+                            icon: Icons.sports_gymnastics,
+                            taskName: 'Gym',
+                            subTitle: 'Sports',
+                            color: Colors.green,
+                          ),
+                          TasksTile(
+                            icon: Icons.dinner_dining,
+                            taskName: 'Dinner',
+                            subTitle: 'Appoitment',
+                            color: Colors.red,
+                          ),
+                          TasksTile(
+                            icon: Icons.dinner_dining,
+                            taskName: 'Dinner',
+                            subTitle: 'Appoitment',
+                            color: Colors.red,
+                          ),
+                        ],
+                      )),
+                      SizedBox(
+                        height: 8,
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(width: 20.0),
-                Text(
-                  'Omar',
-                  style: TextStyle(
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-          SizedBox(height: 40.0),
-          SizedBox(
-            height: 280.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length + 1,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) {
-                  return SizedBox(width: 80.0);
-                }
-                return _buildCategoryCard(
-                  index - 1,
-                  'Notes',
-                  4,
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 15.0),
-            child: TabBar(
-              controller: _tabController,
-              labelColor: Colors.black,
-              unselectedLabelColor: Color(0xFFAFB4C6),
-              indicatorColor: Color.fromARGB(255, 2, 11, 30),
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorWeight: 4.0,
-              isScrollable: true,
-              tabs: <Widget>[
-                Tab(
-                  child: Text(
-                    'Notes',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 20.0),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 30.0),
-            padding: EdgeInsets.all(30.0),
-            decoration: BoxDecoration(
-              color: Color(0xFFF5F7FB),
-              borderRadius: BorderRadius.circular(30.0),
-            ),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      notes[0].title,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      _timeFormatter.format(notes[0].date),
-                      style: TextStyle(
-                        color: Color(0xFFAFB4C6),
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 15.0),
-                Text(
-                  notes[0].content,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Text(
-                      _dateFormatter.format(notes[0].date),
-                      style: TextStyle(
-                        color: Color(0xFFAFB4C6),
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 20.0),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 30.0),
-            padding: EdgeInsets.all(30.0),
-            decoration: BoxDecoration(
-              color: Color(0xFFF5F7FB),
-              borderRadius: BorderRadius.circular(30.0),
-            ),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      notes[1].title,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      _timeFormatter.format(notes[1].date),
-                      style: TextStyle(
-                        color: Color(0xFFAFB4C6),
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 15.0),
-                Text(
-                  notes[1].content,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
-  }
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties.add(
-        DiagnosticsProperty<TabController>('_tabController', _tabController));
   }
 }
