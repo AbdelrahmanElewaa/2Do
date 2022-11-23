c27a3d08e2e4bce3cdb4f12d03c6e01b5d071e02
Omar Mohamed Abdelrehim <omar1901044@miuegypt.edu.eg>
2022-11-24 00:50:28 +0200

    app bar with back button

--- pages/notes_details.dart
+++ pages/notes_details.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
 
 class NotesDetails extends StatelessWidget {
   const NotesDetails({super.key});
@@ -9,15 +7,37 @@ class NotesDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Column(children: [
-        Text(
-          'Notes',
-          style: TextStyle(
-            fontSize: 28.0,
-            fontWeight: FontWeight.bold,
+          appBar: AppBar(
+            //* back button to notes
+            title: Text("Notes",
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                )),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios_new,
+                  color: Theme.of(context).colorScheme.primary),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
           ),
-        ),
-      ])),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            // ignore: prefer_const_literals_to_create_immutables
+            child: Column(children: [
+              // ignore: prefer_const_constructors
+              Text(
+                'Orders',
+                // ignore: prefer_const_constructors
+                style: TextStyle(
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ]),
+          )),
     );
   }
 }
