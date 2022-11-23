fc1b85e951ad06c816bb13792d9317ff583dffdb
Omar Mohamed Abdelrehim <omar1901044@miuegypt.edu.eg>
2022-11-23 22:45:31 +0200

    notes tile util page

---
+++ util/notes_tile.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../util/drop_down.dart';
+
+class NotesTile extends StatelessWidget {
+  final String taskName;
+  final String subTitle;
+  final color;
+  const NotesTile({
+    Key? key,
+    required this.taskName,
+    required this.subTitle,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 214, 193, 130),
+            borderRadius: BorderRadius.circular(16)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: 12,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //title
+                    Text(
+                      taskName,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    //sub title
+                    Text(subTitle,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Colors.red,
+                        )),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
