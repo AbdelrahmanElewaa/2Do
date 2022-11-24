f293be476b33cebea75f0215dfff833884317eb9
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-11-24 23:37:29 +0200

    login-signup validation

--- pages/signup.dart
+++ pages/signup.dart
@@ -4,8 +4,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:todo/pages/login.dart';
-
 import '../util/sizedboxx.dart';
 import '../util/textt.dart';
 
@@ -17,6 +15,7 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,114 +32,142 @@ class _SignupPageState extends State<SignupPage> {
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBoxx(h: 10.0),
-              //*Signup text
-              Textt(
-                text: 'Sign up',
-                size: 60,
-              ),
-              SizedBoxx(h: 10.0),
-              //*Create account text
-              Textt(
-                text: 'Create a free account',
-                size: 20,
-              ),
-              SizedBoxx(h: 20.0),
-              //*email textfield
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: TextField(
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Email',
+          child: Form(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //*Signup text
+                Textt(
+                  text: 'Sign up',
+                  size: 50,
+                ),
+                SizedBoxx(h: 10.0),
+                //*Create account text
+                Textt(
+                  text: 'Create a free account',
+                  size: 20,
+                ),
+                SizedBoxx(h: 20.0),
+                //*email textfield
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: TextFormField(
+                        //-Validation for email
+                        validator: (value) {
+                          if (value != null && value.length < 7) {
+                            return 'Enter minmum 7 characters';
+                          } else {
+                            return null;
+                          }
+                        },
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Email',
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBoxx(h: 12.0),
-              //*password textfield
+                SizedBoxx(h: 12.0),
+                //*password textfield
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: TextField(
-                      obscureText: true,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Password',
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: TextFormField(
+                        //-password validation
+                        validator: (value) {
+                          if (value != null && value.length < 7) {
+                            return 'Enter minmum 7 characters';
+                          } else {
+                            return null;
+                          }
+                        },
+                        obscureText: true,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Password',
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBoxx(h: 12.0),
-              //*confirm password textfield
+                SizedBoxx(h: 12.0),
+                //*confirm password textfield
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      border: Border.all(color: Theme.of(context).primaryColor),
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: TextField(
-                      obscureText: true,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Confirm Password',
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        border:
+                            Border.all(color: Theme.of(context).primaryColor),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: TextFormField(
+                        //- confirm password validation
+                        validator: (value) {
+                          if (value != null && value.length < 7) {
+                            return 'Enter minmum 7 characters';
+                          } else {
+                            return null;
+                          }
+                        },
+                        obscureText: true,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Confirm Password',
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBoxx(h: 4.0),
-              //*signup button
-              Container(
-                padding: const EdgeInsets.all(25),
-                child: SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            Theme.of(context).colorScheme.secondary,
-                        minimumSize: const Size.fromHeight(70), // NEW
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15))),
-                    onPressed: () {},
-                    child: GestureDetector(
-                      onTap: () {
-                        GoRouter.of(context).go('/login');
+                SizedBoxx(h: 4.0),
+                //*signup button
+                Container(
+                  padding: const EdgeInsets.all(25),
+                  child: SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary,
+                          minimumSize: const Size.fromHeight(70), // NEW
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                      onPressed: () {
+                        final isValidForm = formKey.currentState!.validate();
+                        if (isValidForm) {
+                          GoRouter.of(context).go('/login');
+                        }
                       },
                       child: Text(
                         'Sign up',
@@ -149,31 +176,31 @@ class _SignupPageState extends State<SignupPage> {
                     ),
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //* already have an account text
-                  Textt(
-                    text: "Already have an account ? ",
-                    size: 17,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      GoRouter.of(context).go('/login');
-                    },
-                    //* login switch text
-                    child: Text('Login',
-                        style: GoogleFonts.bebasNeue(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
-                          color: Theme.of(context).colorScheme.secondary,
-                          decoration: TextDecoration.underline,
-                        )),
-                  ),
-                ],
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //* already have an account text
+                    Textt(
+                      text: "Already have an account ? ",
+                      size: 17,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        GoRouter.of(context).go('/login');
+                      },
+                      //* login switch text
+                      child: Text('Login',
+                          style: GoogleFonts.bebasNeue(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                            color: Theme.of(context).colorScheme.secondary,
+                            decoration: TextDecoration.underline,
+                          )),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
