5c3f77d50f97ff4913e266b02a29806a2555d66d
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-11-24 14:55:14 +0200

    tasks color change

--- Model/TasksModel.dart
+++ Model/TasksModel.dart
@@ -21,84 +21,77 @@ class TodoItem extends StatelessWidget {
   TextStyle? _getTextStyle(bool checked) {
     if (!checked) {
       return const TextStyle(
-      color: Colors.amber,
-      // decoration: TextDecoration.lineThrough,
-    );
+        color: Colors.black,
+        // decoration: TextDecoration.lineThrough,
+      );
     }
 
     return const TextStyle(
-      color: Colors.amber,
+      color: Colors.black,
       decoration: TextDecoration.lineThrough,
-decorationThickness: 3,
+      decorationThickness: 3,
       decorationColor: Colors.amber,
     );
-
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return 
-      
-      Dismissible(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Dismissible(
         key: ObjectKey(todo),
         onDismissed: (direction) {
           todos.remove(todo);
         },
         background: Container(color: Colors.red),
         child: Card(
-          elevation: 3,
+          elevation: 0,
           // color: Color.fromRGBO(10, 145, 171, 1),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20.0),
           ),
-          color: Color.fromRGBO(6, 84, 113, 1),
+          color: Theme.of(context).primaryColor,
           // color: Colors.amber,
 
           child: ListTile(
             onLongPress: () {
-
               // GoRouter.of(context).go('/addtask');
               Navigator.of(context).push(
                 MaterialPageRoute(
-                  builder: (context) =>   EditTask(todo: todo),
+                  builder: (context) => EditTask(todo: todo),
                 ),
               );
-
             },
-              //
-          onTap: () {
-            onTodoChanged(todo);
-          },
+            //
+            onTap: () {
+              onTodoChanged(todo);
+            },
             leading: Icon(
-                color: Colors.amber,
+                color: Theme.of(context).iconTheme.color,
                 Icons.note_alt_outlined),
             // Text()
 
+            // leading: CircleAvatar(
+            //   child: Text(todo.name[0]),
+            // ),
 
-          // leading: CircleAvatar(
-          //   child: Text(todo.name[0]),
-          // ),
-
-          title: Text(todo.name, style: _getTextStyle(todo.checked)),
+            title: Text(todo.name, style: _getTextStyle(todo.checked)),
             // value: todo.checked,
             // onChanged: (bool? value) {
             //   onTodoChanged(todo);
             // },
-          //
-          //  checkboxShape: ContinuousRectangleBorder(),
-
-
-    ),
+            //
+            //  checkboxShape: ContinuousRectangleBorder(),
+          ),
         ),
-      );
+      ),
+    );
   }
 }
 
 class TodoList extends StatefulWidget {
   @override
-  TodoListState createState() =>  TodoListState();
-
+  TodoListState createState() => TodoListState();
 }
 
 class TodoListState extends State<TodoList> {
@@ -106,55 +99,49 @@ class TodoListState extends State<TodoList> {
 
   @override
   void initState() {
-
     todos;
-
   } // final List<Todo> _todos = <Todo>[];
-
 
   @override
   Widget build(BuildContext context) {
     // inittodos();
-    return Scaffold(
-      appBar:  AppBar(
-        title:  Text('Todo list'),
-// primary: ,
-
-      ),
-      body:
-      // ListView.builder(
-      //   itemCount: todos.length,
-      //     itemBuilder: (context, index) {
-      //    final item=  todos[index],
-      //    todos.map((Todo todo) {
-      //             return TodoItem(
-      //               todo: todo,
-      //               onTodoChanged: handleTodoChange,
-      //             );
-      //     },
-      //      Dismissible(key: ObjectKey(item),
-      //          child: child
-      //      )
-      //
-      // )
-      ListView(
-        physics: BouncingScrollPhysics(),
-        padding: EdgeInsets.symmetric(vertical: 8.0),
-        children: todos.map((Todo todo) {
-          return TodoItem(
-            todo: todo,
-            onTodoChanged: handleTodoChange,
-          );
-        }).toList(),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () =>
-        GoRouter.of(context).go('/addtask'),
-          tooltip: 'Add Item',
-          child: Icon(Icons.add)
+    return SafeArea(
+      child: Scaffold(
+        body:
+            // ListView.builder(
+            //   itemCount: todos.length,
+            //     itemBuilder: (context, index) {
+            //    final item=  todos[index],
+            //    todos.map((Todo todo) {
+            //             return TodoItem(
+            //               todo: todo,
+            //               onTodoChanged: handleTodoChange,
+            //             );
+            //     },
+            //      Dismissible(key: ObjectKey(item),
+            //          child: child
+            //      )
+            //
+            // )
+            ListView(
+          physics: BouncingScrollPhysics(),
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          children: todos.map((Todo todo) {
+            return TodoItem(
+              todo: todo,
+              onTodoChanged: handleTodoChange,
+            );
+          }).toList(),
+        ),
+        floatingActionButton: FloatingActionButton(
+            elevation: 0.0,
+            onPressed: () => GoRouter.of(context).go('/addtask'),
+            tooltip: 'Add Item',
+            child: Icon(Icons.add)),
       ),
     );
   }
+
   // void inittodos(){
   //   addTodoItem("sdfsd");
   // }
