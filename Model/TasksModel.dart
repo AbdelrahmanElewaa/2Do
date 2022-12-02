580dd6ca2b952d252e451d8c888936893555098a
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2022-12-02 20:46:48 +0200

    options in listview

--- Model/TasksModel.dart
+++ Model/TasksModel.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:todo/Data/TasksData.dart';
 import 'dart:math';
-
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:todo/Model/AddTask.dart';
 import 'package:todo/Model/taskDescription.dart';
 import 'package:todo/pages/home_page.dart';
@@ -38,12 +38,86 @@ class TodoItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: Dismissible(
+      child: Slidable(
         key: ObjectKey(todo),
-        onDismissed: (direction) {
-          todos.remove(todo);
-        },
-        background: Container(color: Colors.red),
+          endActionPane: ActionPane(
+            motion: const ScrollMotion(),
+            dismissible: DismissiblePane(onDismissed: ()
+            {
+              todos.remove(todo);
+              const snackBar = SnackBar(
+                content: Text('Item successfully deleted!!'),
+                backgroundColor:  Color.fromARGB(255, 71, 181, 255),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }
+            ),
+            children:   [
+              SlidableAction(
+                onPressed: (context) {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => EditTask(todo: todo),
+                    ),
+                  );
+                },
+                backgroundColor: Color.fromARGB(255, 71, 181, 255),
+                foregroundColor: Colors.white,
+                icon: Icons.edit,
+                label: 'Edit',
+              ),
+              // A SlidableAction can have an icon and/or a label.
+              SlidableAction(
+                onPressed: (context) {
+                  todos.remove(todo);
+                  // Noti
+                },
+                backgroundColor: Color(0xFFFE4A49),
+                foregroundColor: Colors.white,
+                icon: Icons.delete,
+                label: 'Delete',
+              ),
+
+            ],
+        ),
+
+        startActionPane:  ActionPane(
+          motion: ScrollMotion(),
+          children: [
+            SlidableAction(
+              // An action can be bigger than the others.
+              flex: 1,
+              onPressed: (context) {
+      // Navigator.of(context).push(
+      // MaterialPageRoute(
+      // builder: (context) => EditTask(todo: todo),
+      // ),
+      // );
+      },
+              backgroundColor: Color(0xFF7BC043),
+              foregroundColor: Colors.white,
+              icon: Icons.archive,
+              label: 'Archive',
+            ),
+            SlidableAction(
+              onPressed:  (context) {
+          // Navigator.of(context).push(
+          // MaterialPageRoute(
+          // builder: (context) => EditTask(todo: todo),
+          // ),
+          // );
+      },
+              backgroundColor: Color(0xFF0392CF),
+              foregroundColor: Colors.white,
+              icon: Icons.save,
+              label: 'Save',
+            ),
+          ],
+        ),
+        // onDismissed: (direction) {
+        //   todos.remove(todo);
+        // },
+        // background: Container(color: Colors.red),
         child: Card(
           elevation: 0,
           // color: Color.fromRGBO(10, 145, 171, 1),
