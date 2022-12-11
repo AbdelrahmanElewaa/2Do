e8988b50f4409aada66ec7aae8c1fae849bb6f0d
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-12-11 22:45:17 +0200

    Routing

---
+++ Routing/app_routing.dart
@@ -0,0 +1,72 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../Model/AddTask.dart';
+import '../Model/TasksModel.dart';
+import '../Model/start.dart';
+import '../Widgets/splashscreen.dart';
+import '../pages/account.dart';
+import '../pages/home_page.dart';
+import '../pages/login.dart';
+import '../pages/notes_details.dart';
+import '../pages/settings.dart';
+import '../pages/signup.dart';
+import '../pages/stat.dart';
+
+final GoRouter router = GoRouter(
+  routes: <GoRoute>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) => start(),
+      routes: <GoRoute>[
+        GoRoute(
+          path: 'addtask',
+          builder: (BuildContext context, GoRouterState state) => AddTask(),
+        ),
+        GoRoute(
+          path: 'stat',
+          builder: (BuildContext context, GoRouterState state) => Stat(),
+        ),
+        GoRoute(
+          path: 'settings',
+          builder: (BuildContext context, GoRouterState state) => Settings(),
+        ),
+        GoRoute(
+          path: 'account',
+          builder: (BuildContext context, GoRouterState state) => Account(),
+        ),
+        GoRoute(
+          path: 'login',
+          builder: (BuildContext context, GoRouterState state) => LoginPage(),
+        ),
+        GoRoute(
+          path: 'signup',
+          builder: (BuildContext context, GoRouterState state) => SignupPage(),
+        ),
+        GoRoute(
+          path: 'TodoList',
+          builder: (BuildContext context, GoRouterState state) => TodoList(),
+        ),
+        GoRoute(
+          path: 'splash',
+          builder: (BuildContext context, GoRouterState state) =>
+              SplashScreen(),
+        ),
+        GoRoute(
+          path: 'notesdetails',
+          builder: (BuildContext context, GoRouterState state) =>
+              NotesDetails(),
+        ),
+        GoRoute(
+          path: 'home/:selectedIndex',
+          name: 'home',
+          builder: (context, state) => HomePage(
+            selectedIndex: int.parse(state.params["selectedIndex"]!),
+          ),
+        ),
+      ],
+    ),
+  ],
+);
--- main.dart
+++ main.dart
@@ -1,81 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:todo/Model/TasksModel.dart';
-import 'package:todo/Model/start.dart';
-import 'package:todo/Model/taskDescription.dart';
-import 'package:todo/Widgets/splashscreen.dart';
-import 'package:todo/Model/AddTask.dart';
-import 'package:todo/pages/home_page.dart';
-import 'package:todo/pages/login.dart';
-import 'package:todo/pages/settings.dart';
-import 'package:todo/pages/account.dart';
-import 'package:todo/pages/signup.dart';
-import 'package:todo/pages/stat.dart';
-import 'package:todo/pages/notes_details.dart';
-import 'package:go_router/go_router.dart';
-
+import 'Routing/app_routing.dart';
 import 'helper/notificationservice.dart';
 
 class TodoApp extends StatelessWidget {
   //* Application routing
-  final GoRouter router = GoRouter(
-    routes: <GoRoute>[
-      GoRoute(
-        path: '/',
-        builder: (BuildContext context, GoRouterState state) => start(),
-        routes: <GoRoute>[
-          GoRoute(
-            path: 'addtask',
-            builder: (BuildContext context, GoRouterState state) => AddTask(),
-          ),
-          GoRoute(
-            path: 'stat',
-            builder: (BuildContext context, GoRouterState state) => Stat(),
-          ),
-          GoRoute(
-            path: 'settings',
-            builder: (BuildContext context, GoRouterState state) => Settings(),
-          ),
-          GoRoute(
-            path: 'account',
-            builder: (BuildContext context, GoRouterState state) => Account(),
-          ),
-          GoRoute(
-            path: 'login',
-            builder: (BuildContext context, GoRouterState state) => LoginPage(),
-          ),
-          GoRoute(
-            path: 'signup',
-            builder: (BuildContext context, GoRouterState state) =>
-                SignupPage(),
-          ),
-          GoRoute(
-            path: 'TodoList',
-            builder: (BuildContext context, GoRouterState state) => TodoList(),
-          ),
-          GoRoute(
-            path: 'splash',
-            builder: (BuildContext context, GoRouterState state) =>
-                SplashScreen(),
-          ),
-          GoRoute(
-            path: 'notesdetails',
-            builder: (BuildContext context, GoRouterState state) =>
-                NotesDetails(),
-          ),
-          GoRoute(
-              path: 'home/:selectedIndex',
-            name: 'home',
-            builder: (context, state) => HomePage(
-                selectedIndex: int.parse(state.params["selectedIndex"]!),
 
-          ),
-          ),
-        ],
-      ),
-    ],
-  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
@@ -103,4 +34,5 @@ class TodoApp extends StatelessWidget {
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   NotificationService().initNotification();
-  runApp(TodoApp());}
+  runApp(TodoApp());
+}
