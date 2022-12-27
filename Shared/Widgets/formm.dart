f1c2de1a5d3e997493b89ba9ae8aad024e2d3852
Omar Mohamed Abdelrehim <omar1901044@miuegypt.edu.eg>
2022-12-27 20:35:50 +0200

    hint text in formm class

--- Shared/Widgets/formm.dart
+++ Shared/Widgets/formm.dart
@@ -1,25 +1,27 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
+
 class Formm extends StatelessWidget {
+  final htext;
   const Formm({
     Key? key,
+    this.htext,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      //* Email textfield
       child: Container(
         decoration: BoxDecoration(
-            color: Color.fromARGB(255, 255, 255, 255),
+            color: Theme.of(context).primaryColor,
             border: Border.all(color: Colors.white),
             borderRadius: BorderRadius.circular(15)),
         child: Padding(
           padding: const EdgeInsets.only(left: 15),
           child: TextFormField(
-            //-Validation for email
+            //-Validation 
             validator: (value) {
               if (value != null && value.length < 7) {
                 return 'Enter minmum 7 characters';
@@ -35,7 +37,7 @@ class Formm extends StatelessWidget {
             // ignore: prefer_const_constructors
             decoration: InputDecoration(
               border: InputBorder.none,
-              hintText: 'Email',
+              hintText: htext,
             ),
           ),
         ),
