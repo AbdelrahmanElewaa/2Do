9f33722c4d81ad7102d7792045ff244be5728f39
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-12-27 23:12:29 +0200

    calendar colors

--- Statistics/View/stat.dart
+++ Statistics/View/stat.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import 'package:todo/Statistics/Widgets/piechart.dart';
-
 
 import '../../Shared/Widgets/sizedboxx.dart';
 // import '../../Common widgets/sizedboxx.dart';
@@ -42,11 +42,38 @@ class _StatState extends State<Stat> {
 
                   Container(
                     decoration: BoxDecoration(
-                      color: Colors.white70,
-                      borderRadius: BorderRadius.circular(20)
-                    ),
-                    height: 395,
+                        color: Colors.white70,
+                        borderRadius: BorderRadius.circular(20)),
+                    height: MediaQuery.of(context).size.height / 2.3,
                     child: TableCalendar(
+                      calendarFormat: CalendarFormat.month,
+                      weekendDays: const [
+                        DateTime.friday,
+                        DateTime.saturday,
+                      ],
+                      calendarStyle: CalendarStyle(
+                        
+                        weekendTextStyle: TextStyle(
+                            color: Color.fromARGB(179, 0, 255, 34),
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                        defaultTextStyle: TextStyle(
+                            color: Color.fromARGB(179, 0, 47, 255),
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      daysOfWeekStyle: DaysOfWeekStyle(
+                          weekendStyle: TextStyle(
+                              color: Color.fromARGB(255, 105, 0, 243),
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold),
+                          weekdayStyle: TextStyle(
+                              color: Color.fromARGB(255, 255, 197, 25),
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold)),
+                      headerStyle: HeaderStyle(
+                          titleTextStyle:
+                              TextStyle(color: Color.fromARGB(255, 255, 0, 0))),
                       firstDay: DateTime.utc(2010, 10, 16),
                       lastDay: DateTime.utc(2030, 3, 14),
                       focusedDay: DateTime.now(),
