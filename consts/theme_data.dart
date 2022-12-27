ff5b74d4877519732ed82df000dd96168421f25c
Abdelrahman Elewa <abdelrahmanelewa6@gmail.com>
2022-12-27 15:52:27 +0200

    Update theme_data.dart

--- consts/theme_data.dart
+++ consts/theme_data.dart
@@ -2,19 +2,38 @@ import 'package:flutter/material.dart';
 
 class Styles {
   static ThemeData themeData(bool isDarkTheme) {
-    return ThemeData(
+    return isDarkTheme ?ThemeData(
 
 
          splashColor: Color(0xFF000000),
         secondaryHeaderColor: Color.fromARGB(255, 94, 94, 94),
-        primaryColor:isDarkTheme ? Colors.grey[700]: Colors.white,
-        scaffoldBackgroundColor: isDarkTheme ? Color(0xFF252525): Colors.white,
+        primaryColor: Colors.grey[700],
+        scaffoldBackgroundColor:  Color(0xFF252525) ,
         colorScheme: ColorScheme.light(
-            primary: isDarkTheme ?Colors.white: Color(0xFF252525),
+            primary: Colors.white,
             secondary: Color.fromARGB(255, 71, 181, 255)),
         iconTheme: IconThemeData(
-          color:isDarkTheme ?Colors.white: Color(0xFF252525),
+          color:Colors.white,
+        ),
+    ):
+
+
+
+ThemeData(
+
+
+         splashColor: Color(0xFF000000),
+        secondaryHeaderColor: Color.fromARGB(255, 94, 94, 94),
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor:  Colors.white,
+        colorScheme: ColorScheme.light(
+            primary: Color(0xFF252525),
+            secondary: Color.fromARGB(255, 71, 181, 255)),
+        iconTheme: IconThemeData(
+          color: Color(0xFF252525),
         ),
     );
+
+
   }
 }
