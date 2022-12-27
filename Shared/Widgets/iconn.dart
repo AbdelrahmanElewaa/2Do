a9a5c2ff0614ac5b77e1f5e20cbb0a9d7f2f2c9b
Omar Mohamed Abdelrehim <omar1901044@miuegypt.edu.eg>
2022-12-27 18:57:14 +0200

    adding parameters to iconn class

--- Shared/Widgets/iconn.dart
+++ Shared/Widgets/iconn.dart
@@ -3,15 +3,20 @@
 import 'package:flutter/material.dart';
 
 class Iconn extends StatelessWidget {
+  final icN;
+  final sizee;
   const Iconn({
     Key? key,
+    required this.icN,
+    this.sizee,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Icon(
-      Icons.arrow_back_ios_new,
+      icN,
       color: Theme.of(context).colorScheme.primary,
+      size: sizee,
     );
   }
 }
