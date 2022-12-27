6f4959cb0baf86d064ff0a3c167289adf5cd46c4
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-12-27 18:07:03 +0200

    notes update

--- Notes/View/notes_details.dart
+++ Notes/View/notes_details.dart
@@ -2,10 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../../Shared/Widgets/textt.dart';
 import '../Data/notes_repository.dart';
+import '../Domain/notes.dart';
 
 class NotesDetails extends StatefulWidget {
   final bool update;
-  const NotesDetails({super.key, this.update = false});
+  final content;
+  final title;
+  final id;
+
+  const NotesDetails(
+      {super.key,
+      this.update = false,
+      required this.title,
+      required this.content,
+      required this.id});
   @override
   State<NotesDetails> createState() => _NotesDetailsState();
 }
@@ -19,6 +29,13 @@ class _NotesDetailsState extends State<NotesDetails> {
   TextEditingController titleController = TextEditingController();
   TextEditingController contentController = TextEditingController();
   @override
+  void initState() {
+    titleController.text = widget.title;
+    contentController.text = widget.content;
+    idUpdateController.text = widget.id;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
@@ -28,26 +45,30 @@ class _NotesDetailsState extends State<NotesDetails> {
             backgroundColor: Colors.transparent,
             elevation: 0,
             leading: IconButton(
-                splashRadius: 20.0,
-                icon: Icon(Icons.arrow_back_ios_new,
-                    color: Theme.of(context).colorScheme.primary),
-                onPressed: () {
-                  if (titleController.text.isNotEmpty ||
-                      contentController.text.isNotEmpty) {
-                    String title = titleController.text;
-                    String content = contentController.text;
-                    String date = '${_dateFormatter.format(DateTime.now())}';
-                    petsRepository.insert(title, content, date).then((id) =>
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('Note Added'))));
-                  }
-                  Navigator.of(context).pop();
-                }),
+              splashRadius: 20.0,
+              icon: Icon(Icons.arrow_back_ios_new,
+                  color: Theme.of(context).colorScheme.primary),
+              onPressed: () {
+                if (titleController.text != widget.title ||
+                    contentController.text != widget.content) {
+                  Pet pet = Pet(
+                      id: int.parse(idUpdateController.text),
+                      title: titleController.text,
+                      content: contentController.text,
+                      date: '${_dateFormatter.format(DateTime.now())}');
+                  petsRepository.update(pet).then((rowsAffected) =>
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('$rowsAffected rows updated'))));
+                }
+                Navigator.of(context).pop();
+              },
+            ),
           ),
           body: Padding(
             padding: const EdgeInsets.all(8.0),
             // ignore: prefer_const_literals_to_create_immutables
             child: Column(children: [
+              Text('${widget.title}'),
               // ignore: prefer_const_constructors
               TextField(
                 controller: titleController,
