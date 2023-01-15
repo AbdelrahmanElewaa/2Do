f0a8539d5cb3124451c0302391fe07196d4c5716
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2023-01-15 08:42:07 +0200

    Create UserFirestore.dart

---
+++ User/Data/UserFirestore.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
+import 'package:todo/User/Data/UserData.dart';
+
+Future<String> createUser({required name,required email,required pw,required mobile, uid}) async {
+  final docUser=FirebaseFirestore.instance.collection('users').doc();
+  final user=Userr(uid: docUser.id,name:name,email: email,mobile: mobile,pw: pw);
+final map=user.toMap();
+await docUser.set(map);
+return docUser.id;
+}
+
+
+Future<String> createUserobj(Userr u) async {
+  final docUser=FirebaseFirestore.instance.collection('users').doc();
+  u.uid=docUser.id;
+  final map=u.toMap();
+await docUser.set(map);
+return docUser.id;
+}
+
+Stream<List<Userr>> readUsers(){
+  return FirebaseFirestore.instance.collection('users').
+  snapshots().map((event) => 
+  event.docs.map((e) => 
+  Userr.fromMap(e.data())).toList());
+}
+
+Future<void> delete(String id) async {
+  await FirebaseFirestore.instance.collection('users').doc(id).delete();
+}
+   
+Future<void> editUser(String id,Userr u) async {
+  await FirebaseFirestore.instance.collection('users').doc(id).update(u.toMap());
+}
