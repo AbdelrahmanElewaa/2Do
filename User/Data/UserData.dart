43feb432a1d1adfe123c24cfc7725a31be57119c
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2023-01-15 08:34:36 +0200

    Update UserData.dart

--- User/Data/UserData.dart
+++ User/Data/UserData.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 List<Userr> Users = <Userr>[];
 
 
@@ -9,5 +11,31 @@ class Userr {
   String mobile;
   String? uid;
 
+Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'pw': pw,
+      'mobile': mobile,
+      'uid': uid
+    };
+  }
+
+  factory Userr.fromMap(Map<String, dynamic> map) {
+    return Userr(
+      name: map['name'] ?? 'not specified',
+      email: map['email'] ?? "not specified",
+      pw: map['pw'] ?? '',
+      mobile: map['mobile'] ?? "not specified",
+      uid: map['uid'] ?? ""
+    );
+  }
+
+  factory Userr.fromJson(String data) {
+    // return   json.decode(data).cast<Todo>();
+    return Userr.fromMap(json.decode(data) as Map<String, dynamic>);
+  }
+
+  String toJson() => json.encode(toMap());
   
 }
