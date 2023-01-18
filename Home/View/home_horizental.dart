9d7806ef966cd19b261a886492083c726340be8b
Abdelrahman Elewa <abdelrahmanelewa6@gmail.com>
2023-01-18 23:21:19 +0200

    Update home_horizental.dart

--- Home/View/home_horizental.dart
+++ Home/View/home_horizental.dart
@@ -9,116 +9,139 @@ import '../Widgets/tasks_tile.dart';
 import '../../Shared/Widgets/calenderweek.dart';
 import 'package:intl/intl.dart';
 import 'package:dismissible_page/dismissible_page.dart';
-class HomeHorizental extends StatelessWidget {
+import '../../Tasks/Data/tasksRepository.dart';
+import '../../Tasks/Data/TasksData.dart';
+import '../../Tasks/Domain/TasksModel2.dart';
+
+class HomeHorizental extends StatefulWidget {
   const HomeHorizental({super.key});
+
+  @override
+  State<HomeHorizental> createState() => _HomeHorizentalState();
+}
+
+class _HomeHorizentalState extends State<HomeHorizental> {
+  final taskrep = TasksRepository.instance;
+  List<Todo> todoss = [];
+  @override
+  void initState() {
+    taskrep.fetchTodoList().then((value) {
+      setState(() {
+        todoss = value;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-          children: [
+      children: [
         //    SizedBoxx(h: 10.0),
-            //greating Row
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        //greating Row
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+              // SizedBoxx(
+              //   h: 10.0,
+              // ),
+              Calenderweek(),
+              //Calender
+
+              // SizedBoxx(
+              //   h: 25.0,
+              // ),
+
+              //Search Bar
+              //  SearchBar(),
+            ],
+          ),
+        ),
+        // SizedBoxx(h: 20.0),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            color: Colors.transparent,
+            child: Center(
               child: Column(
                 children: [
-                  // SizedBoxx(
-                  //   h: 10.0,
-                  // ),
-                  Calenderweek(),
-                  //Calender
+                  // Excercise Heading
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Tasks',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
 
-                  // SizedBoxx(
-                  //   h: 25.0,
-                  // ),
-
-                  //Search Bar
-                //  SearchBar(),
+                      //ADD Button
+                      Hero(
+                        tag: 'unique tag',
+                        child: ElevatedButton(
+                          onPressed: () {
+                            context.pushTransparentRoute(AddTask());
+                            // GoRouter.of(context).go('/addtask');
+                          },
+                          style: ElevatedButton.styleFrom(
+                              shape: CircleBorder(),
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.secondary),
+                          child: Icon(
+                            Icons.add,
+                            size: 40,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  //SizedBoxx(h: 20.0),
+                  //Tasks Dashboard
+                  Expanded(
+                    child: ListView.builder(
+                        padding: const EdgeInsets.all(8),
+                        itemCount: todoss.length + 1,
+                        itemBuilder: (BuildContext context, int index) {
+                          if (index == todoss.length) {
+                            return ElevatedButton(
+                              child: const Text('Refresh'),
+                              onPressed: () {
+                                taskrep.fetchTodoList().then((value) {
+                                  setState(() {
+                                    todoss = value;
+                                  });
+                                });
+                              },
+                            );
+                          }
+                          return TasksTile(
+                            icon: Icons.dinner_dining,
+                            taskName: '${todoss[index].name}',
+                            subTitle: '${todoss[index].cat}',
+                            color: Colors.red,
+                          );
+                        }),
+                    // SizedBoxx(
+                    //   h: 8.0,
+                  ) // ),
                 ],
               ),
             ),
-           // SizedBoxx(h: 20.0),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                color: Colors.transparent,
-                child: Center(
-                  child: Column(
-                    children: [
-                      // Excercise Heading
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Tasks',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                          ),
-
-                          //ADD Button
-                          Hero(
-                            tag: 'unique tag',
-                            child: ElevatedButton(
-                              onPressed: () {
-                                context.pushTransparentRoute(AddTask());
-                                // GoRouter.of(context).go('/addtask');
-                              },
-                              style: ElevatedButton.styleFrom(
-                                  shape: CircleBorder(),
-                                  backgroundColor:
-                                      Theme.of(context).colorScheme.secondary),
-                              child: Icon(
-                                Icons.add,
-                                size: 40,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      //SizedBoxx(h: 20.0),
-                      //Tasks Dashboard
-                      Expanded(
-                          child: ListView(
-                        // ignore: prefer_const_literals_to_create_immutables
-                        children: [
-                          TasksTile(
-                            icon: Icons.work,
-                            taskName: 'Project Meeting',
-                            subTitle: 'Emergency',
-                            color: Colors.blue,
-                          ),
-                          TasksTile(
-                            icon: Icons.sports_gymnastics,
-                            taskName: 'Gym',
-                            subTitle: 'Sports',
-                            color: Colors.green,
-                          ),
-                          TasksTile(
-                            icon: Icons.dinner_dining,
-                            taskName: 'Dinner',
-                            subTitle: 'Appoitment',
-                            color: Colors.red,
-                          ),
-                          TasksTile(
-                            icon: Icons.dinner_dining,
-                            taskName: 'Dinner',
-                            subTitle: 'Appoitment',
-                            color: Colors.red,
-                          ),
-                        ],
-                      )),
-                      // SizedBoxx(
-                      //   h: 8.0,
-                      // ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        );
+          ),
+        ),
+        // SizedBoxx(
+        //   h: 8.0,
+        // ),
+      ],
+    );
+    //         ),
+    //       ),
+    //     ),
+    //   ],
+    // );
   }
 }
