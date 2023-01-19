08478945e4dc52194df9bd071705db8da226fc30
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-01-19 17:16:03 +0200

    drawer fix

--- Shared/Widgets/drawerr.dart
+++ Shared/Widgets/drawerr.dart
@@ -25,76 +25,74 @@ class _DrawerrState extends State<Drawerr> {
     return Drawer(
       elevation: 0.0,
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      child: Expanded(
-        child: ListView(
-          children: [
-            DrawerrHeader(),
-            ListTile(
-              leading: Icon(Icons.person,
-                  color: Theme.of(context).colorScheme.primary),
-              title: Textt(text: 'My Profile', size: 15.0),
-              onTap: () {
-                GoRouter.of(context).go('/account');
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.login,
-                  color: Theme.of(context).colorScheme.primary),
-              title: Textt(text: 'Login', size: 15.0),
-              onTap: () {
-                GoRouter.of(context).go('/login');
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.logout,
-                  color: Theme.of(context).colorScheme.primary),
-              title: Textt(text: 'Logout', size: 15.0),
-              onTap: () async {
-                // GoRouter.of(context).go('/login');
-                await FirebaseAuth.instance.signOut();
-              },
-            ),
-            Container(
-              child: SwitchListTile(
-                title: Textt(text: 'Theme', size: 15.0),
-                secondary: Icon(
-                  themeState.getDarkTheme
-                      ? Icons.dark_mode_outlined
-                      : Icons.light_mode_outlined,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                onChanged: (bool value) {
-                  setState(() {
-                    themeState.setDarkTheme = value;
-                  });
-                },
-                value: themeState.getDarkTheme,
+      child: ListView(
+        children: [
+          DrawerrHeader(),
+          ListTile(
+            leading: Icon(Icons.person,
+                color: Theme.of(context).colorScheme.primary),
+            title: Textt(text: 'My Profile', size: 15.0),
+            onTap: () {
+              GoRouter.of(context).go('/account');
+            },
+          ),
+          ListTile(
+            leading:
+                Icon(Icons.login, color: Theme.of(context).colorScheme.primary),
+            title: Textt(text: 'Login', size: 15.0),
+            onTap: () {
+              GoRouter.of(context).go('/login');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout,
+                color: Theme.of(context).colorScheme.primary),
+            title: Textt(text: 'Logout', size: 15.0),
+            onTap: () async {
+              // GoRouter.of(context).go('/login');
+              await FirebaseAuth.instance.signOut();
+            },
+          ),
+          Container(
+            child: SwitchListTile(
+              title: Textt(text: 'Theme', size: 15.0),
+              secondary: Icon(
+                themeState.getDarkTheme
+                    ? Icons.dark_mode_outlined
+                    : Icons.light_mode_outlined,
+                color: Theme.of(context).colorScheme.primary,
               ),
+              onChanged: (bool value) {
+                setState(() {
+                  themeState.setDarkTheme = value;
+                });
+              },
+              value: themeState.getDarkTheme,
             ),
-            Column(
-              children: [
-                GestureDetector(
-                  child: Container(
-                    width: 120.0,
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                            width: 4.0,
-                            color: Theme.of(context).colorScheme.secondary),
-                        borderRadius: BorderRadius.circular(30.0)),
-                    child: Center(
-                        child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 5.0),
-                      child: Textt(size: 25.0, text: 'Logout'),
-                    )),
-                  ),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
+          ),
+          Column(
+            children: [
+              GestureDetector(
+                child: Container(
+                  width: 120.0,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 4.0,
+                          color: Theme.of(context).colorScheme.secondary),
+                      borderRadius: BorderRadius.circular(30.0)),
+                  child: Center(
+                      child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Textt(size: 25.0, text: 'Logout'),
+                  )),
                 ),
-              ],
-            )
-          ],
-        ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
