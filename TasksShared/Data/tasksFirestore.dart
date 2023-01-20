552abbc8c3856e7b34793ad118bb25cca72f4856
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2023-01-20 07:26:00 +0200

    sharedwith in add sharedtask

--- TasksShared/Data/tasksFirestore.dart
+++ TasksShared/Data/tasksFirestore.dart
@@ -3,13 +3,31 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:todo/TasksShared/Data/sharedTasksData.dart';
 import 'dart:convert';
 
-Future<String> createTask({required String name, required String checked, required String cat, required String rem, required String shared, required String des,required String uid}) async {
+Future<String> createTask({required String name, required String checked, 
+required String cat, required String rem, required String shared, 
+required String des,required String uid,required String sharedwith,String taskid=' '}) async {
+  // SharedTodo task;
+  if (taskid==' '){
   final docTask=FirebaseFirestore.instance.collection('users').doc(uid).collection('tasks').doc();
-  final task=SharedTodo(id: docTask.id,name:name,checked: checked,description: des,reminder: rem,cat: cat,shared: shared,sharedwith: shared );
-final map=task.toMap();
+
+  SharedTodo task=SharedTodo(id: docTask.id,name:name,checked: checked,
+  description: des,reminder: rem,cat: cat,shared: shared,sharedwith: sharedwith );
+  final map=task.toMap();
 await docTask.set(map);
 return docTask.id;
+  }
+  else{
+  final docTask=FirebaseFirestore.instance.collection('users').doc(uid).collection('tasks').doc(taskid);
+
+  SharedTodo task=SharedTodo(id: taskid,name:name,checked: checked,
+  description: des,reminder: rem,cat: cat,shared: shared,sharedwith: sharedwith );
+ 
+final map=task.toMap();
+await docTask.set(map);
+return taskid;
+  }
 }
+
 
 
 Future<String> createTaskobj(SharedTodo todo,String uid) async {
