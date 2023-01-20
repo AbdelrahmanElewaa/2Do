45fe9accf757b0722cabe97f25d939a3e89a5e56
Omar Mohamed Abdelrehim <omar1901044@miuegypt.edu.eg>
2023-01-20 22:08:09 +0200

    automaticcly update statistics on selected date

--- Statistics/View/stat_vertical.dart
+++ Statistics/View/stat_vertical.dart
@@ -1,14 +1,15 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unnecessary_import, implementation_imports
 
-import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import 'package:todo/Statistics/Widgets/piechart.dart';
 
 import '../../Shared/Widgets/sizedboxx.dart';
+import '../../Tasks/Data/TasksData.dart';
+import '../../Tasks/Data/tasksRepository.dart';
 import 'utils.dart';
 
 class StatVertical extends StatefulWidget {
@@ -21,7 +22,35 @@ class StatVertical extends StatefulWidget {
 class _StatVerticalState extends State<StatVertical> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
-  DateTime? _selectedDay;
+  DateTime? _selectedDay = null;
+
+  final taskrep = TasksRepository.instance;
+  List<Todo> todoss = [];
+  double checked = 0;
+  @override
+  void initState() {
+    taskrep.fetchTodoList().then((value) {
+      setState(() {
+        if (_selectedDay == null) {
+          todoss = value;
+        } else {
+          for (int i = 0; i < value.length; i++) {
+            if (value[i].reminder.substring(0, 10) ==
+                DateFormat('yyyy-MM-dd').format(_selectedDay!)) {
+              todoss.add(value[i]);
+            }
+          }
+        }
+
+        for (int i = 0; i < todoss.length; i++) {
+          if (todoss[i].checked == "true") {
+            checked++;
+          }
+        }
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +76,36 @@ class _StatVerticalState extends State<StatVertical> {
                   },
                   onDaySelected: (selectedDay, focusedDay) {
                     setState(() {
-                      Pie(s: _selectedDay);
+                      todoss = [];
+                      checked = 0;
+                      taskrep.fetchTodoList().then((value) {
+                        setState(() {
+                          if (_selectedDay == null) {
+                            todoss = value;
+                          } else {
+                            print(
+                                DateFormat('yyyy-MM-dd').format(_selectedDay!));
+
+                            for (int i = 0; i < value.length; i++) {
+                              if (value[i].reminder.substring(0, 10) ==
+                                  DateFormat('yyyy-MM-dd')
+                                      .format(_selectedDay!)) {
+                                todoss.add(value[i]);
+                              }
+                            }
+                          }
+
+                          for (int i = 0; i < todoss.length; i++) {
+                            if (todoss[i].checked == "true") {
+                              checked++;
+                            }
+                          }
+                        });
+                      });
+                      Pie(
+                          s: _selectedDay,
+                          list: todoss.length.toDouble(),
+                          checked: checked);
                       _selectedDay = selectedDay;
                       _focusedDay = focusedDay;
                     });
@@ -68,7 +126,11 @@ class _StatVerticalState extends State<StatVertical> {
           ),
           SizedBoxx(h: 20.0),
 
-          Expanded(child: Pie(s: _selectedDay)),
+          Expanded(
+              child: Pie(
+                  s: _selectedDay,
+                  list: todoss.length.toDouble(),
+                  checked: checked)),
         ],
       ),
     );
